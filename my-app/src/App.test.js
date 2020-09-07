f32ae23783bda20f6e5864f964e5d48ce74f2a2c
“sjq” <3023493319@qq.com>
2020-09-07 09:53:28 +0800

    提交

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import Adapter from 'enzyme-adapter-react-16'
+import App from './App'
+// mount集成测试 全渲染；  shallow 单元测试 当前组件渲染
+import Enzyme, { mount } from 'enzyme'
+Enzyme.configure({
+  adapter: new Adapter(),
+})
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it('render without crashing', () => {
+  const wrapper = mount(<App />)
+  const w = wrapper.find('[data-test="container"]')
+  //测试代码 解耦
+  expect(w).toExist()
+  // console.log(wrapper.debug())
+  expect(w).toHaveProp('title', 'dell lee')
+})
